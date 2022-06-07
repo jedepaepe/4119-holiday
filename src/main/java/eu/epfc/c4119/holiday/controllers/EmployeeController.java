dd2d63de90334f70ef91d2f553368602f8bfef4d
jedepaepe <jedepaepe@epfc.eu>
2022-06-07 18:07:16 +0200

    test

--- src/main/java/eu/epfc/c4119/holiday/controllers/EmployeeController.java
+++ src/main/java/eu/epfc/c4119/holiday/controllers/EmployeeController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+/**
+ * traite les requêtes http
+ */
 @Controller
 public class EmployeeController {
     private EmployeeRepository repository;
